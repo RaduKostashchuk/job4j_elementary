6712d12c54e70efd153799aa6c46258447d6de8e
RaduKostashchuk <kostasc@mail.ru>
2021-08-04 16:10:15 +0700

    6.7.4. Выигрышные комбинации в сокобан [#53859]

--- test/ru/job4j/array/MatrixCheckTest.java
+++ test/ru/job4j/array/MatrixCheckTest.java
@@ -63,4 +63,43 @@ import org.junit.Assert;
             char[] expected = {'X', 'Y', 'Z'};
             Assert.assertArrayEquals(expected, result);
         }
+
+        @Test
+        public void whenDataMonoByTrueThenTrue() {
+            char[][] input = {
+                    {' ', ' ', 'X', ' ', ' '},
+                    {' ', ' ', 'X', ' ', ' '},
+                    {' ', ' ', 'X', ' ', ' '},
+                    {' ', ' ', 'X', ' ', ' '},
+                    {' ', ' ', 'X', ' ', ' '},
+            };
+            boolean result = MatrixCheck.isWin(input);
+            Assert.assertTrue(result);
+        }
+
+        @Test
+        public void whenDataNotMonoByTrueThenFalse() {
+            char[][] input = {
+                    {' ', ' ', 'X', ' ', ' '},
+                    {' ', ' ', 'X', ' ', ' '},
+                    {' ', 'X', ' ', ' ', ' '},
+                    {' ', ' ', 'X', ' ', ' '},
+                    {' ', ' ', 'X', ' ', ' '},
+            };
+            boolean result = MatrixCheck.isWin(input);
+            Assert.assertFalse(result);
+        }
+
+        @Test
+        public void whenDataHMonoByTrueThenTrue() {
+            char[][] input = {
+                    {' ', ' ', ' ', ' ', ' '},
+                    {' ', ' ', ' ', ' ', ' '},
+                    {'X', 'X', 'X', 'X', 'X'},
+                    {' ', ' ', 'X', ' ', ' '},
+                    {' ', ' ', 'X', ' ', ' '},
+            };
+            boolean result = MatrixCheck.isWin(input);
+            Assert.assertTrue(result);
+        }
 }
